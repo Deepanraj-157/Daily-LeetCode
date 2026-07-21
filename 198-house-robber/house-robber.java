1a0e629704bea22f89a91b60fe26776994791335
Deepanraj P <deepanp153@gmail.com>
2026-07-21 14:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.73%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,30 @@
 class Solution {
-    public int solve(int i,int[] nums,int[] dp){
-        if(i<0) return 0;
+    // public int solve(int i,int[] nums,int[] dp){
+    //     if(i<0) return 0;
 
-        if(i==0) return nums[0];
-        if(dp[i]!=-1) return dp[i]; 
-        int robCurrent=nums[i]+solve(i-2,nums,dp);
-        int leaveCurrent=solve(i-1,nums,dp);
-        dp[i]=Math.max(robCurrent,leaveCurrent);
-        return dp[i];
-    }
+    //     if(i==0) return nums[0];
+    //     if(dp[i]!=-1) return dp[i]; 
+    //     int robCurrent=nums[i]+solve(i-2,nums,dp);
+    //     int leaveCurrent=solve(i-1,nums,dp);
+    //     dp[i]=Math.max(robCurrent,leaveCurrent);
+    //     return dp[i];
+    // }
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
         int[] dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        dp[0]=nums[0];
-        return solve(nums.length-1,nums,dp);
-
+        int pre2=nums[0];
+        int pre1=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++)
+        {
+            int robCurr=nums[i]+pre2;
+            int leaveCurr=pre1;
+            pre2=pre1;
+            
+            dp[i]=Math.max(robCurr,leaveCurr);
+            pre1=dp[i];
+        }
+        return pre1;
 
         
     }
