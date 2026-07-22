2c9bd5188abe0439a838e6b40b712d83166f87c3
Deepanraj P <deepanp153@gmail.com>
2026-07-22 11:28:00 +0530

    Time: 1 ms (17.18%) | Memory: 43.5 MB (45.73%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -17,8 +17,32 @@ class Solution {
             Arrays.fill(dp[i],-1);
         }
         // dp[0][0]=1;
-        if(obstacleGrid[m-1][n-1]==1) return 0;
-        return CanIReach(0,0,m,n,dp,obstacleGrid);
+        if(obstacleGrid[m-1][n-1]==1 || obstacleGrid[0][0]==1) return 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) {
+                    dp[i][j]=1;
+                    continue;
+                }
+                if( obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i-1>=0 && j-1>=0){
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    
+                }
+                else if(i-1>=0){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else {
+                    dp[i][j]=dp[i][j-1];
+                }
+
+            }
+        }
+        // return CanIReach(0,0,m,n,dp,obstacleGrid);
+        return dp[m-1][n-1];
         
     }
 }
