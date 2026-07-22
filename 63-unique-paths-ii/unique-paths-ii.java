de3445482b080f1221ed364f6819c0e74f52184c
Deepanraj P <deepanp153@gmail.com>
2026-07-22 11:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (11.15%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,22 +1,12 @@
 class Solution {
-    private int CanIReach(int i,int j,int m,int n,int[][] dp,int[][] obstacleGrid){
-        if(i == m-1 && j== n-1) return 1;
-        if(j>=n || i>=m || obstacleGrid[i][j]==1){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j]=CanIReach(i,j+1,m,n,dp,obstacleGrid)+CanIReach(i+1,j,m,n,dp,obstacleGrid);
-        return dp[i][j];
-        
-    }
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        // dp[0][0]=1;
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
         if(obstacleGrid[m-1][n-1]==1 || obstacleGrid[0][0]==1) return 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -28,10 +18,13 @@ class Solution {
                     dp[i][j]=0;
                     continue;
                 }
+                //adding the pre way value
+                //if the curr have two way form the source to curr
                 if(i-1>=0 && j-1>=0){
                     dp[i][j]=dp[i-1][j]+dp[i][j-1];
                     
                 }
+                //if the curr have only one way
                 else if(i-1>=0){
                     dp[i][j]=dp[i-1][j];
                 }
@@ -41,7 +34,6 @@ class Solution {
 
             }
         }
-        // return CanIReach(0,0,m,n,dp,obstacleGrid);
         return dp[m-1][n-1];
         
     }
