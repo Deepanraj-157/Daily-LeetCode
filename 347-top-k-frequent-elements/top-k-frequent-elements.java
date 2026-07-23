ff1b4e830aa248a6d7744f732037829da7315457
Deepanraj P <deepanp153@gmail.com>
2026-07-23 09:50:01 +0530

    Time: 15 ms (55.03%) | Memory: 47.5 MB (66.56%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<int[]> minHeap=new PriorityQueue<>(
-            (a,b)->Integer.compare(a[1],b[1]));
-        // for(int i=0;i<nums.length;i++){
-            
-        // }
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+        PriorityQueue<List<Integer>> pq= new PriorityQueue<>(
+            (a,b)->a.get(1)-b.get(1));
         for(int key:map.keySet()){
-            minHeap.add(new int[]{key,map.get(key)});
-            if(minHeap.size()>k) minHeap.poll();
+            List<Integer> tmp=new ArrayList<>();
+            tmp.add(key);
+            tmp.add(map.get(key));
+            pq.add(tmp);
+            if(pq.size()>k) pq.poll();
 
         }
         int[] ans=new int[k];
         int index=0;
-        while(!minHeap.isEmpty()){
-            int[] tmp=minHeap.poll();
-            ans[index++]=tmp[0];
+        for(List<Integer> list:pq){
+            ans[index++]=list.get(0);
         }
         return ans;
         
