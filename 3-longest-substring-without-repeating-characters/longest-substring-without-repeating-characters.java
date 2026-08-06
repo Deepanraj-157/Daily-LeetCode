6030861ea79deffcd4678a79f63df5f1cfb6866c
Deepanraj P <deepanp153@gmail.com>
2026-08-06 11:54:18 +0530

    Time: 180 ms (5.07%) | Memory: 47.8 MB (9.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,18 +3,15 @@ class Solution {
         Queue<Character> q=new LinkedList<>();
         int max=0;
         for(char c:s.toCharArray()){
-            // System.out.println("Char = "+c);
             if(!q.contains(c)){
                 q.offer(c);
                 max=Math.max(max,q.size());
-                // System.out.print(q+" ");
             }
             else{
                 while(q.contains(c)) q.poll();
                  q.offer(c);
             }
            
-            // System.out.print(q+" ");
         }
         return max;
 
