3ce6d00a0166ec0a04520884538d11402e774984
Deepanraj P <deepanp153@gmail.com>
2026-08-18 15:04:52 +0530

    Time: 3 ms (54.76%) | Memory: 45.3 MB (24.14%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -3,7 +3,7 @@ class Solution {
     public void addElement(List<Integer> arr,int i,int[] nums){
         if(i==nums.length){
             List<Integer> newList=new ArrayList<>(arr);
-            Collections.sort(newList);
+            // Collections.sort(newList);
             set.add(new ArrayList<>(newList));
             return;
         } 
@@ -15,6 +15,7 @@ class Solution {
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<Integer> tmp=new ArrayList<>();
+        Arrays.sort(nums);
         addElement(tmp,0,nums);
         
         return new ArrayList<>(set);     
