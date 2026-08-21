72ecfaa5644393a8294c2609b79e310c180b2abd
Deepanraj P <deepanp153@gmail.com>
2026-08-21 11:01:31 +0530

    Time: 2 ms (95.96%) | Memory: 45.6 MB (68.30%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+
+        return res;
+    }
+
+    private void makeCombination(
+            int[] candidates,
+            int target,
+            int idx,
+            List<Integer> comb,
+            int total,
+            List<List<Integer>> res) {
+
+        if (total == target) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        if (total > target || idx >= candidates.length) {
+            return;
+        }
+
+        // Take current element
+        comb.add(candidates[idx]);
+
+        // idx stays same because we can reuse the element
+        makeCombination(
+            candidates,
+            target,
+            idx,
+            comb,
+            total + candidates[idx],
+            res
+        );
+
+        // Backtrack
+        comb.remove(comb.size() - 1);
+
+        // Skip current element
+        makeCombination(
+            candidates,
+            target,
+            idx + 1,
+            comb,
+            total,
+            res
+        );
+    }
+}
